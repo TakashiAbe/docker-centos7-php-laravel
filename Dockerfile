41bec7f85d39da59326eefdd296022dcaa7b6637
Takashi Abe <d77d77@gmail.com>
2017-02-03 02:30:57 +0900

    no message

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM takashiabe/centos7-apache-php
+RUN yum clean all && yum -y update
+
+RUN mkdir /var/www_tmp &&  cd /var/www_tmp && composer create-project laravel/laravel laravel
+
+COPY ./docker-entrypoint.sh /
+RUN chmod +x /docker-entrypoint.sh
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+EXPOSE 80
+CMD ["/usr/sbin/init"]
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -e /var/www/laravel ]; then
+  mv /var/www_tmp/laravel /var/www
+  rm -rf /var/www_tmp
+  cd /var/www && ln -s /var/www/laravel/public html
+  chmod -R a+w /var/www/laravel/storage/
+  chmod -R a+w /var/www/laravel/bootstrap/cache/
+  sed -i -e "s/'DB_HOST', 'localhost'/'DB_HOST', '${MYSQL_PORT_3306_TCP_ADDR}'/g" /var/www/laravel/config/database.php
+  sed -i -e "s/'DB_PORT', '3306'/'DB_PORT', '${MYSQL_PORT_3306_TCP_PORT}'/g" /var/www/laravel/config/database.php
+  sed -i -e "s/'DB_DATABASE', 'forge'/'DB_DATABASE', '${MYSQL_ENV_MYSQL_DATABASE}'/g" /var/www/laravel/config/database.php
+  sed -i -e "s/'DB_USERNAME', 'forge'/'DB_USERNAME', '${MYSQL_ENV_MYSQL_USER}'/g" /var/www/laravel/config/database.php
+  sed -i -e "s/'DB_PASSWORD', ''/'DB_PASSWORD', '${MYSQL_ENV_MYSQL_ROOT_PASSWORD}'/g" /var/www/laravel/config/database.php
+  sed -i -e "s/'charset' => 'utf8'/'charset' => 'utf8mb4'/g" /var/www/laravel/config/database.php
+fi
+
+if [ -e /var/www/html ]; then
+  umount /var/www/html
+  rm -rf /var/www/html
+  cd /var/www && ln -s /var/www/laravel/public html
+  chmod -R a+w /var/www/laravel/bootstrap/cache
+fi
+
+exec "$@"
